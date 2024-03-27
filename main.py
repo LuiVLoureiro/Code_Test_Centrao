43fc319a1225a4b8f60e5662677a4dcd300259f6
LuiLoureiro <luivloureiro@gmail.com>
2024-03-27 15:03:42 -0300

    fix: Conectar banco em todas as requisicoes

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ class Database:
     def __init__(self):
         pass
 
-    def criar():
+    def conectar():
+        # Conectar com a Database
         global conn
         global cursor
         conn = sql.connect('centrao.db')
         cursor = conn.cursor()
+
+    def criar():
+        # Conectar com a Database
+        Database.conectar()
+        
         cursor.execute(
             '''CREATE TABLE IF NOT EXISTS Usuario (
             ID INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -29,6 +35,9 @@ class Database:
         conn.commit()
 
     def criar_dados_falsos():
+        # Conectar com a Database
+        Database.conectar()
+
         fake = Faker('pt_BR')
         cargos = ['Estágio', 'Junior', 'Pleno', 'Senior']
         sexos = ['Homem', 'Mulher']
@@ -97,6 +106,8 @@ class Database:
             cursor.execute(sql_insert, values)
 
     def visualizar_database():
+        # Conectar com a Database
+        Database.conectar()
         # Executar consulta para selecionar todos os registros da tabela Usuario
         cursor.execute('SELECT * FROM Usuario')
 
@@ -113,6 +124,6 @@ class Database:
             print("A tabela está vazia.")
 
 #Se já tiver criado a Database, comentar linha abaixo!
-Database.criar()
-Database.criar_dados_falsos()
+#Database.criar()
+#Database.criar_dados_falsos()
 Database.visualizar_database()
